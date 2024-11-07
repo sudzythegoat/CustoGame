3f62b42489b86f273513046e42b941e2aaf03229
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-07 12:17:07 -0600

    Create custom.js

---
+++ custom/src/custom.js
@@ -0,0 +1,7 @@
+let levelupper = prompt("What level would you like to start on?");
+let level = levelupper.toLowerCase();
+if (level !== null) {
+  let clevel = "inital";
+} else if (level) {
+  let clevel = level;
+}
