63805d0341ec06ec48c4f122ad0091ba3e3dbc4a
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-07 12:24:15 -0600

    Update custom.js

--- custom/src/custom.js
+++ custom/src/custom.js
@@ -5,3 +5,18 @@ if (level !== null) {
 } else if (level) {
   let clevel = level;
 }
+let startingtechup = prompt("Starting Weapon")
+let startingtech = startingtechup.toLowerCase();
+if (startingtech !== null) {
+  let cweapon = "";
+} else if (level) {
+  let cweapon = startingtech;
+}
+let startingsheildup = prompt("Starting Field")
+let startingsheild = startingsheildup.toLowerCase();
+if (startingsheild !== null) {
+  let csheild = "";
+} else if (startingsheild) {
+  let csheild = startingsheild;
+}
+function main
