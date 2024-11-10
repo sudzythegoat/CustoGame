0563c60f22f96227f82dbc4e50969dda20ca416e
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-10 10:25:07 -0600

    Update custom.js

--- custom/src/custom.js
+++ custom/src/custom.js
@@ -1,30 +1,18 @@
 let levelupper = prompt("What level would you like to start on?");
-let level = levelupper.toLowerCase();
-if (level !== null) {
-  let clevel = "inital";
-} else if (level) {
-  let clevel = level;
-}
-let startingtechup = prompt("Starting Weapon")
-let startingtech = startingtechup.toLowerCase();
-if (startingtech !== null) {
-  let cweapon = "";
-} else if (startingtech) {
-  let cweapon = startingtech;
-}
-let startingsheildup = prompt("Starting Field")
-let startingsheild = startingsheildup.toLowerCase();
-if (startingsheild !== null) {
-  let csheild = "";
-} else if (startingsheild) {
-  let csheild = startingsheild;
-}
-if (clevel !== "") {
-  level.onLevel(clevel);
+let clevel = levelupper ? levelupper.toLowerCase() : "initial";
+
+let startingtechup = prompt("Starting Weapon");
+let cweapon = startingtechup ? startingtechup.toLowerCase() : "";
+
+let startingsheildup = prompt("Starting Field");
+let csheild = startingsheildup ? startingsheildup.toLowerCase() : "";
+
+if (clevel !== "initial") {
+  console.log(`level.onLevel('${clevel}');`);
 }
 if (cweapon !== "") {
-  b.giveGuns(cweapon);
+  console.log(`b.giveGuns('${cweapon}');`);
 }
 if (csheild !== "") {
-  m.setField(csheild);
+  console.log(`m.setField('${csheild}');`);
 }
